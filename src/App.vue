ac48a4f0b86c8c0fe8db444092596e43004f25f2
Helio Brito <helioh3@gmail.com>
2022-04-17 22:56:24 -0300

    feature: add project with vue-cli studies

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="container mt-5">
+      <h2 class="text-center mb-5">Carrinho de compra</h2>
+
+      <cart :products="products"/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Cart from '@/components/cart/Cart.vue'
+export default {
+  data () {
+    return {
+      products: [
+        {id: 1,  phone: 'iphone 12pro', color: 'spacegray', amount: 7000, qty: 1},
+        {id: 2,  phone: 'iphone 11pro', color: 'gold share', amount: 6000, qty: 1},
+        {id: 3,  phone: 'iphone 10pro', color: 'green', amount: 5000, qty: 1},
+        {id: 4,  phone: 'iphone 9pro', color: 'gold', amount: 4000, qty: 1},
+        {id: 5,  phone: 'galaxy s22', color: 'black', amount: 8000, qty: 1}
+      ]
+    }
+  },
+  name: 'App',
+  components: {
+    Cart
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import { createRouter, createWebHashHistory } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+// const routes = [
+//   { path:'/', components: helloWorld }
+// ]
+//
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes,
+// })
+
+createApp(App)
+    .mount('#app')
